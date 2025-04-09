2860c13f52730932db72a1579c24d684a844a4df
AngelinaVybornova <vybornova.03@mail.ru>
2025-04-09 12:00:15 +0300

    add LogWriter

---
+++ testTask/ClassLibrary1/LogWriter.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Logger
+{
+	public class LogWriter
+	{
+		private readonly string _path;
+		private readonly LoggerOptions _options;
+
+		public LogWriter(string path, LoggerOptions options)
+		{
+			_path = path;
+			_options = options;
+		}
+
+		public void Write(string message)
+		{
+			Directory.CreateDirectory(Path.GetDirectoryName(_path));
+			using (StreamWriter writer = new StreamWriter(_path, true, _options.encoding))
+			{
+				writer.WriteLine(message);
+			}
+		}
+	}
+}
